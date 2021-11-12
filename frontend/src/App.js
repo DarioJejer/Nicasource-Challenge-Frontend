a2ed9a1c360a7064f3ed624dc9b2887aefd47e38
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-12 03:15:45 -0300

    Added grid layout for Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import * as React from "react";
-import { Home } from './components/Home';
+import { Home } from './components/Home/Home';
 import { Route, Routes } from "react-router-dom";
 
 function App() {
---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <div class="grid-container">
+      <div>Home Page 1</div>
+      <div>Home Page 2</div>
+      <div>Home Page 3</div>
+      <div>Home Page 4</div>
+      <div>Home Page 5</div>
+      <div>Home Page 6</div>
+      <div>Home Page 7</div>
+    </div>
+  );
+};
