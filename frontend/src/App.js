69117bf952e7b8841b83bf587a672fb98592c1ef
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-12 02:39:03 -0300

    Setted up React app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import * as React from "react";
+import { Home } from './components/Home';
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
